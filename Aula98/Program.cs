9543112c600c9c279900de257f2f3a6b7d356a5a
Linas Janiques <fillipe.janiques@gmail.com>
2021-01-23 12:49:30 -0300

    Acrescentada mensagem de buenos dias

--- Aula98/Program.cs
+++ Aula98/Program.cs
@@ -7,6 +7,7 @@ namespace Aula98
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Buenos dias");
         }
     }
 }
